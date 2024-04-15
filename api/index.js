b7b86d09ae952ee77ec5ac99c8733ab877b98062
Jacob1010-h <lordgaming1010@gmail.com>
2024-04-15 13:05:35 -0700

    more CORS

--- api/index.js
+++ api/index.js
@@ -5,7 +5,12 @@ import teamsRouter from './routes/teams.js';
 
 const app = express();
 app.use(express.json());
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use('/', landingRouter);
 app.use('/data', dataRouter);
 app.use('/teams', teamsRouter);
