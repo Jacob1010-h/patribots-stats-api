552703c928afbdaa35f05a229c3795e4bd4c2849
Jacob1010-h <lordgaming1010@gmail.com>
2024-04-15 13:08:50 -0700

    change THE CORS

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import teamsRouter from './routes/teams.js';
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', false);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
