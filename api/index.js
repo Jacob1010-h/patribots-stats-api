bdd8a7638770180cf3b1568367c9fbc81ddfb660
Jacob1010-h <lordgaming1010@gmail.com>
2024-04-15 12:32:52 -0700

    add cors headers?

--- api/index.js
+++ api/index.js
@@ -6,9 +6,28 @@ import teamsRouter from './routes/teams.js';
 const app = express();
 app.use(express.json());
 
+app.options('/', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.end();
+});
+app.options('/data', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.end();
+});
+app.options('/teams', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.end();
+});
 app.use('/', landingRouter);
 app.use('/data', dataRouter);
 app.use('/teams', teamsRouter);
+
 
 const PORT = process.env.PORT || 3030;
 
