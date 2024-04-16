2401c3861243c9968b0ce1b9c962bde6a7c5e312
Jacob1010-h <lordgaming1010@gmail.com>
2024-04-16 13:02:00 -0700

    all all back

--- api/routes/landing.js
+++ api/routes/landing.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { fetchDataAndProcess } from '../database.js';
+import { fetchDataAndProcess, fetchDataAndProcessAll } from '../database.js';
 
 var router = express.Router();
 
